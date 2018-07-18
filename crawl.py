a331192f9e19d0bee60c0a39db9f9ef1d84bb3bb
Tran Sang <tranvansangk41@gmail.com>
2018-07-18 17:11:24 +0900

    more verbose

--- crawl.py
+++ crawl.py
@@ -6,7 +6,16 @@ import time
 
 base_url = 'https://news.zing.vn/tra-cuu-diem-thpt-2018.html'
 #time interval in seconds
-time_interval = 0.3
+time_interval = 0.5
+max_retry = 10
+retry_interval = 1
+pref_start = 1
+pref_end = 63
+sbd_id_start = 0
+sbd_id_end = 99999
+last_fail_pref = 1
+last_fail_sbd_id = 499
+verbose_range = 10
 
 #sbd: string
 def crawl(sbd):
@@ -31,8 +40,7 @@ def crawl(sbd):
             student[subject] = score
         return student
     except:
-        print("Unexpected error when crawling %s: " % sbd, sys.exc_info()[0])
-        print(res.text, res, res.status_code)
+        print("Unexpected error when crawling %s. Status code = %d" % (sbd, res.status_code))
         raise
         return None
 
@@ -65,20 +73,32 @@ def crawl_and_write(pref, sbd_id):
         return True
     return False
 
-def scan_all(pref_start, pref_end, sbd_id_start, sbd_id_end):
-    for pref in range(pref_start, pref_end + 1):
-        found = False
-        for sbd_id in range(sbd_id_start, sbd_id_end + 1):
-            try:
-                if crawl_and_write(pref, sbd_id):
-                    found = True
-                time.sleep(time_interval)
-            except:
-                return
-        if not found:
-            print("No student data in pref code %d\n" % pref)
+def scan_all(pref_start, pref_end, sbd_id_start, sbd_id_end, last_fail_pref, last_fail_sbd_id):
+    for pref in range(max(pref_start, last_fail_pref), pref_end + 1):
+        cnt = 0
+        if pref == last_fail_pref:
+            start_index = max(last_fail_sbd_id, sbd_id_start)
+        else:
+            start_index = sbd_id_start
+        print("Start crawling pref id %d" % pref)
+        for sbd_id in range(start_index, sbd_id_end + 1):
+            retry = 0
+            if (sbd_id % verbose_range == 0):
+                print("crawling student (%d, %d) (cnt = %d)" % (pref, sbd_id, cnt))
+            while retry < max_retry:
+                try:
+                    if crawl_and_write(pref, sbd_id):
+                        cnt += 1
+                    retry = max_retry
+                except:
+                    retry += 1
+                    time.sleep(retry_interval)
+                    print("Crawling student (%d, %d) failed, retry %d" %(pref, sbd_id, retry))
+            time.sleep(time_interval)
+        if cnt == 0:
+            print("No student data in pref code %d" % pref)
 
-scan_all(1, 63, 0, 99999)
+scan_all(pref_start, pref_end, sbd_id_start, sbd_id_end, last_fail_pref, last_fail_sbd_id)
 # crawl_and_write(63, 100)
 # crawl_and_write(63, 100)
 row = ['prefecture', 'student_id']
