ba314b1966116b0480f70662a72c02645897ccca
Tran Sang <tranvansangk41@gmail.com>
2018-07-18 16:19:34 +0900

    run test ok

--- crawl.py
+++ crawl.py
@@ -26,9 +26,53 @@ def crawl(sbd):
         student[subject] = score
     return student
 
-for pref in 
-print(crawl('63000100'))
+pref_start = 1
+pref_end = 63
+sbd_id_start = 0
+sbd_id_end = 99999
+
 csvfile = open('out.csv', 'wb')
 csvwriter = csv.writer(csvfile)
-csvwriter.writerow(['hello', '', 'myworld'])
-csvwriter.writerow(['another hello', None, 'myworld'])
+subjects = {}
+
+def force_set(lst, idx, val):
+    while len(lst) <= idx:
+        lst.append('')
+    lst[idx] = val
+    return lst
+
+def write_student(pref, sbd_id, student):
+    row = [pref, sbd_id]
+    for subject, score in  student.iteritems():
+        if subject in subjects:
+            index = subjects[subject]
+        else:
+            index = len(subjects)
+            subjects[subject] = index
+        force_set(row, index + 2, score)
+    csvwriter.writerow(row)
+
+def crawl_and_write(pref, sbd_id):
+    sbd = "%2d0%05d" % (pref, sbd_id)
+    student = crawl(sbd)
+    if (student != None):
+        write_student(pref, sbd_id, student)
+        return True
+    return False
+
+def scan_all(pref_start, pref_end, sbd_id_start, sbd_id_end):
+    for pref in range(pref_start, pref_end + 1):
+        found = False
+        for sbd_id in range(sbd_id_start, sbd_id_end + 1):
+            if crawl_and_write(pref, sbd_id):
+                found = True
+        if not found:
+            print("No student data in pref code %d\n" % pref)
+
+# scan_all(1, 63, 0, 99999)
+crawl_and_write(63, 100)
+crawl_and_write(63, 100)
+row = ['prefecture', 'student_id']
+for subject, index in subjects.iteritems():
+    force_set(row, index + 2, subject)
+csvwriter.writerow(row)
